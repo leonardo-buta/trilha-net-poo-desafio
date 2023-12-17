b1f9d821ba11c8a98c3b4a0bed3683039b404b43
Herbert Emidio <herbertemidio2017@gmail.com>
2023-12-17 12:09:36 -0300

    Entrega do projeto Nokia e iPhone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,26 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
+        protected string Numero { get; set; }
+
+
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
+
 
         public Smartphone(string numero)
         {
             Numero = numero;
+        }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria) : this(numero)
+        {
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
